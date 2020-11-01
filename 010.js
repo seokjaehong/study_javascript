9f112f4d490c9cb0a144882ef8eee285dd9164f5
seokjae hong <hsj2334@algorithmlabs.co.kr>
2020-11-01 19:13:35 +0900

    조건문, 반복문

---
+++ 010.js
@@ -0,0 +1,42 @@
+/* 20201101
+조건문 
+*/
+
+
+// var result=true;
+// if(result) console.log(result);
+// if(!result)
+//     console.log(result);
+
+// if(result){
+//     console.log('result의 결과');
+//     console.log('>>참입니다 ')
+// }
+
+
+// var number=2;
+// if(number==1){
+//     console.log('number is 1')
+// }
+// else if (number==2){
+//     console.log("number is 2")
+// }else if (number==3){
+//     console.log("number is 3")
+// }
+// else{
+//     console.log('not correct')
+// }
+
+
+var subject = '자바스크립트';
+switch(subject){
+    case 'c언어':
+        console.log("c++");
+        break;
+    case '자바스크립트':
+        console.log('javascript');
+        break;
+    default:
+        console.log('end')
+        break;
+}
---
+++ 013.js
@@ -0,0 +1,34 @@
+/* 20201101
+반복문
+*/
+
+// for(var i=0;i<10;i++){
+//     console.log(i+"번쨰 반복분 ")
+// }
+
+// var hometown=[
+//     {name:"남준", place:"일산", city:"고양"},
+//     {name:"진",place:"과천"},
+//     {name:"호석",place:"광주",city:"전라도"},
+//     {name:"지민",place:"부산",city:"경상도"},
+// ];
+
+// for(var i=0; i< hometown.length; i++){
+//     var h = hometown[i];
+//     if (!h| !h.city) continue;
+//         console.log(i+'번쨰 실행입니다');
+
+//     if (h.name==='호석'){
+//         console.log(h.name +'의 고향은'+h.city+''+h.place+'입니다');
+//         break;
+//     }    
+// }
+
+
+var store = {snack:1000, flower:50000, beverage:20000};
+
+for (var item in store) {
+    if(!store.hasOwnProperty(item)) continue;
+
+    console.log(item + '는 가격이 '+ store[item]+ '입니다.');
+}
